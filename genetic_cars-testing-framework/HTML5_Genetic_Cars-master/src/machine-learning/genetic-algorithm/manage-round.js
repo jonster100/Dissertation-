06a87a5e395f5a03f529bd68c6e129b026f89628
jonster100 <jon_james97@hotmail.com>
2019-02-14 19:49:49 +0000

    Changed Code Organization
    Seperated the Evolutionary algorithm into the baseline one and my-own implementation.

--- genetic_cars-testing-framework/HTML5_Genetic_Cars-master/src/machine-learning/genetic-algorithm/manage-round.js
+++ genetic_cars-testing-framework/HTML5_Genetic_Cars-master/src/machine-learning/genetic-algorithm/manage-round.js
@@ -25,44 +25,60 @@ function generationZero(config){
   };
 }
 
-function nextGeneration(
-  previousState,
-  scores,
-  config
-){
+//--------------------------------------------------------------------------- my code job64
+
+function runEA(scores, config){
+	scores.sort(function(a, b){return a.score.s - b.score.s;});
+	var schema = config.schema;//list of car variables i.e "wheel_radius", "chassis_density", "vertex_list", "wheel_vertex"
+	var newGeneration = new Array();
+	for (var k = 0; k < 10; k++) {
+		var parents=new Array();
+		parents.push(selection.runSelection(scores,2,true).def);
+		scores.splice(scores.findIndex(x=> x.index===parents[0].index),1);
+		parents.push(selection.runSelection(scores,1,false).def);
+		scores.splice(scores.findIndex(x=> x.index===parents[1].index),1);
+		var newCars = crossover.runCrossover(parents,0,config.schema);
+		for(var i=0;i<2;i++){
+			newCars[i].is_elite = false;
+			newCars[i].index = k;
+			newGeneration.push(mutation.mutate(newCars[i],schema));
+		}
+	}	
+	return newGeneration;
+}
+
+function runBaselineEA(scores, config){
+	scores.sort(function(a, b){return a.score.s - b.score.s;});
+	var schema = config.schema;//list of car variables i.e "wheel_radius", "chassis_density", "vertex_list", "wheel_vertex"
+	var newGeneration = new Array();
+	
+	console.log(scores);//test data
+	for (var k = 0; k < 20; k++) {
+		newGeneration.push(mutation.mutate(scores[i].def,schema));
+		newGeneration[i].is_elite = false;
+		newGeneration[i].index = k;
+	}
+}	
+
+function nextGeneration(previousState, scores, config){
   var champion_length = config.championLength,
     generationSize = config.generationSize,
     selectFromAllParents = config.selectFromAllParents;
 	scores.sort(function(a, b){return a.score.s - b.score.s;});
 	var schema = config.schema;//list of car variables i.e "wheel_radius", "chassis_density", "vertex_list", "wheel_vertex"
-  var newGeneration = new Array();
-  var newborn;
-  console.log("Log -- "+previousState.counter);
-  console.log(scores);//test data
-  for (var k = 0; k < 10; k++) {
-	var parents=new Array();
-	parents.push(selection.rouleteWheelSel(scores).def);
-	scores.splice(scores.findIndex(x=> x.index===parents[0].index),1);
-	parents.push(selection.rouleteWheelSel(scores).def);
-	scores.splice(scores.findIndex(x=> x.index===parents[1].index),1);
-	var newCars = crossover.runCrossover(parents,0,config.schema);
-	for(var i=0;i<2;i++){
-		newCars[i].is_elite = false;
-		newCars[i].index = k;
-		newGeneration.push(mutation.mutate(newCars[i],schema));
-	}
-	//var newCar = mutation.mutate(selParent,schema);
-    //newGeneration.push(newCar.def);
-	
-  }
+	var newGeneration = new Array();
+	var newborn;
+	console.log("Log -- "+previousState.counter);
+	console.log(scores);//test data
+	newGeneration = runEA(scores,config);
 	console.log(newGeneration);//test data
-
   return {
     counter: previousState.counter + 1,
     generation: newGeneration,
   };
 }
 
+//------------------------------------------------------------------------------ end of my code job64
 
 
 function makeChild(config, parents){
--- genetic_cars-testing-framework/HTML5_Genetic_Cars-master/src/machine-learning/genetic-algorithm/selection.js
+++ genetic_cars-testing-framework/HTML5_Genetic_Cars-master/src/machine-learning/genetic-algorithm/selection.js
@@ -1,6 +1,19 @@
 
 module.exports = {
-	rouleteWheelSel: rouleteWheelSel
+	runSelection: runSelection
+}
+/*
+This function changes the type of selection used depending on the parameter number "selectType" = (rouleteWheelSel - 1, tournamentSelection - 2)
+@param boolean strongest this parameter is passed through to the tournamentSelection function where true is return the strongest and false get weakest
+@param int selectType this parameter determines the type of selection used.
+@param Array carsArr this paramet is the population which the selection functions are used on. */
+function runSelection(carsArr, selectType, strongest){
+	if(selectType===1){
+		return rouleteWheelSel(carsArr);
+	} 
+	else if(selectType===2){
+		return tournamentSelection(carsArr,strongest);
+	}
 }
 
 function rouleteWheelSel(carsArr){
@@ -25,5 +38,10 @@ function rouleteWheelSel(carsArr){
 	else{
 		return carsArr[0];
 	}
+}
+
+function tournamentSelection(carsArr, strongest){
+	carsArr.sort(function(a,b){return (strongest===true)?b.score.s - a.score.s:a.score.s - a.score.b;});
+	return carsArr[0];
 }
 
