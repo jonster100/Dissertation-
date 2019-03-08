b6e2b8f42617b608232851c04f41326a35b5625b
jonster100 <jon_james97@hotmail.com>
2019-03-08 20:59:08 +0000

    Fixed selection undefined error due to decreasing inital population size

--- genetic_cars-testing-framework/HTML5_Genetic_Cars-master/src/generation-config/index.js
+++ genetic_cars-testing-framework/HTML5_Genetic_Cars-master/src/generation-config/index.js
@@ -6,7 +6,7 @@ var schema = carConstruct.generateSchema(carConstants);
 var pickParent = require("./pickParent");
 var selectFromAllParents = require("./selectFromAllParents");
 const constants = {
-  generationSize: 40,
+  generationSize: 20,
   schema: schema,
   championLength: 1,
   mutation_range: 1,
